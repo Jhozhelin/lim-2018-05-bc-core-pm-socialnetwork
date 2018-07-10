cc7d553a9013dc37d6e1df76f4e45c391c862cad
MargotRivera <margotimeldarb@gmail.com>
2018-07-10 18:01:54 -0500

    Primeros cambios

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyAyf_oyKwPWoDgRXxskVnPxYKGCwGbVANA",
+    authDomain: "login-red-social.firebaseapp.com",
+    databaseURL: "https://login-red-social.firebaseio.com",
+    projectId: "login-red-social",
+    storageBucket: "login-red-social.appspot.com",
+    messagingSenderId: "870954229021"
+};
+firebase.initializeApp(config);
+
