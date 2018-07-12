23a8f98db9e87fbdaf8decb4a48302092ac5770f
MargotRivera <margotimeldarb@gmail.com>
2018-07-12 16:41:49 -0500

    Subiendo cambios

--- src/main.js
+++ src/main.js
@@ -1,22 +1,61 @@
 /*Elementos*/
-const box = document.getElementById('containerLogin');
-const button = document.getElementById('buttonEnter');
-const logininput = document.getElementById('loginEmail').value;
-const loginpassword = document.getElementById('loginEmail').value;
+const login = document.getElementById('buttonEnter');
 const register = document.getElementById('buttonregister');
-const boxWellcome = document.getElementById('wellcome');
 
-register.addEventListener('click', register =()=>{
-  const createUser = document.getElementById('createEmail').value;
-  const createPassword = document.getElementById('createPassword').value;
-  firebase.auth().createUserWithEmailAndPassword(createUser, createPassword)
-    .then(register => {
-      boxWellcome.style = "display:block";
-    })
-  .catch(function (error) {
+
+register.addEventListener('click', () => {
+  const emailRegister = document.getElementById('email').value;
+  const createPassword = document.getElementById('createPassword').value;  
+  firebase.auth().createUserWithEmailAndPassword(emailRegister, createPassword)
+     .catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       // ...
-    })
+      /*console.log(errorCode)
+      console.log(errorMessage)*/
+    });
 })
+
+login.addEventListener('click', () => {
+  const loginEmail = document.getElementById('loginEmail').value;
+  const loginPassword = document.getElementById('loginPassword').value;
+  const boxMessage = document.getElementById('message');
+  const boxLogin = document.getElementById('boxLogin');
+  firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
+  .then(response=>{
+    boxLogin.style.display = 'none';
+    boxMessage.style.display = 'block';
+  })
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode)
+      console.log(errorMessage)
+      // ...
+    });
+})
+
+
+const observ = () => {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      console.log('Existe usuario')
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
+      console.log('No existe usuario')
+    }
+  });
+}
+observ();
