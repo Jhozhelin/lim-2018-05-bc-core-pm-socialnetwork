579b7d4fa4dcd59285fd0424518fb7a3189bacc2
Jhozhelin <jhozhelinzuniga@gmail.com>
2018-08-21 16:32:44 -0500

    teminando la funcionalidad de like

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 //***********Función writeUserData */
 window.writeUserData = (userId, name, email, imageUrl) => {
-  // console.log(userId, name, email, imageUrl);
+  // console.log(userId, name, email, imageUrl)
 
   firebase.database().ref('users/' + userId).set({
     username: name,
@@ -8,7 +8,7 @@ window.writeUserData = (userId, name, email, imageUrl) => {
     profile_picture: imageUrl
   }).then(result => {
     location.href = 'muro.html'
-  });
+  })
 }
 //**********Función para registrar usuario*************
 window.register = (email, password) => {
@@ -16,15 +16,15 @@ window.register = (email, password) => {
     .auth()
     .createUserWithEmailAndPassword(email, password)
     .then(result => {
-      console.log(result);
+      console.log(result)
       writeUserData(result.user.uid, null, result.user.email, null)
     })
     .catch(function (error) {
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      var errorCode = error.code
+      var errorMessage = error.message
       // ...
-    });
+    })
 }
 
 //**********Función para loguear usuario**********
@@ -33,74 +33,74 @@ window.signIn = (email, password) => {
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then(result => {
-      console.log(result);
+      console.log(result)
 
       location.href = 'muro.html'
     })
     .catch(function (error) {
-      console.log(error);
+      console.log(error)
 
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      var errorCode = error.code
+      var errorMessage = error.message
       // ...
-    });
+    })
 }
 
 //**********Función para loguearse con google**********
 
 window.google = () => {
-  var provider = new firebase.auth.GoogleAuthProvider();
+  var provider = new firebase.auth.GoogleAuthProvider()
   firebase
     .auth()
     .signInWithPopup(provider)
     .then(function (result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
+      var token = result.credential.accessToken
       // The signed-in user info.
-      var user = result.user;
+      var user = result.user
       writeUserData(user.uid, user.displayName, user.email, user.photoURL)
     })
     .catch(function (error) {
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      var errorCode = error.code
+      var errorMessage = error.message
       // The email of the user's account used.
-      var email = error.email;
+      var email = error.email
       // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+      var credential = error.credential
       console.log(error)
       // ...
-    });
+    })
 }
 
 //**********Función para loguearse con facebook**********
 window.facebook = () => {
-  var provider = new firebase.auth.FacebookAuthProvider();
+  var provider = new firebase.auth.FacebookAuthProvider()
   firebase
     .auth()
     .signInWithPopup(provider)
     .then(function (result) {
       console.log(result)
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
+      var token = result.credential.accessToken
       // The signed-in user info.
-      var user = result.user;
+      var user = result.user
       // ...
       writeUserData(user.uid, user.displayName, user.email, user.photoURL)
 
     }).catch(function (error) {
       // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
+      var errorCode = error.code
+      var errorMessage = error.message
       // The email of the user's account used.
-      var email = error.email;
+      var email = error.email
       // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+      var credential = error.credential
       // ...
-      console.log(error);
+      console.log(error)
 
-    });
+    })
 }
 
 //**********Función para salir de la app**********
@@ -109,87 +109,85 @@ window.logout = () => {
     .auth()
     .signOut()
     .then(function () {
-      console.log('Cerro Sesión');
+      console.log('Cerro Sesión')
     }).catch(function (error) {
-      console.log('Error al cerrar Sesión');
-    });
+      console.log('Error al cerrar Sesión')
+    })
 }
 
 //**********Función para crear post**********
 window.writeNewPost = (postData) => {
-  console.log(postData);
+  console.log(postData)
   // A post entry.
   // Get a key for a new Post.
-  const newPostKey = firebase.database().ref().child('posts').push().key;
+  const newPostKey = firebase.database().ref().child('posts').push().key
   // Write the new post's data simultaneously in the posts list and the user's post list.
-  const updates = {};
-  updates['/posts/' + newPostKey] = postData;
-  updates['/user-posts/' + postData.uid + '/' + newPostKey] = postData;
-  firebase.database().ref().update(updates);
-  return newPostKey;
+  const updates = {}
+  updates['/posts/' + newPostKey] = postData
+  updates['/user-posts/' + postData.uid + '/' + newPostKey] = postData
+  firebase.database().ref().update(updates)
+  return newPostKey
 }
 
 //**********Función para traers posts**********
 window.getPost = () => firebase.database().ref('/posts/').once('value')
 
+
 //**********Función para editar post**********
 window.editPost = (userId, text, postId) => {
-  console.log(userId, text, postId);
+  console.log(userId, text, postId)
 
   firebase.database().ref('user-posts/' + userId + '/' + postId)
     .update({
       body: text
-    });
+    })
 
   firebase.database().ref('posts/' + postId)
     .update({
       body: text
-    });
+    })
 }
 
 //**********Función para eliminar post**********
 window.deletePost = (userId, postId) => {
-  console.log(userId);
-  console.log(postId);
+  console.log(userId)
+  console.log(postId)
 
 
   firebase.database().ref().child('/user-posts/' + userId + '/' + postId)
-    .remove();
+    .remove()
   firebase.database().ref().child('posts/' + postId)
-    .remove();
+    .remove()
 }
 
 
-
 // ****************funcion de likes ***************
-window.likePost = (starCount, userId, postId) =>{
+window.likePost = (userId, postId) =>{
+  console.log(postId)
+  let contLike = null
+  firebase.database().ref(`/posts/${postId}`).once('value').then(result=>{
+  contLike = result.val().starCount + 1
+
+  console.log(contLike)
+
+    
+
  firebase.database().ref('user-posts/' + userId + '/postId')
  .update({
-   starCount: like
- });
+   starCount: contLike
+ })
  firebase.database().ref('posts/' + postId)
  .update({
-  starCount: like
- });
+  starCount: contLike
+ })
+  
+    
+  }). catch(error => {
+    console.log(error)
+    
+  })
 
-};
+  console.log(contLike)
 
 
-// //**********Función para contar los likes***********
-// window.likePosts = (userId, postId, ) => {
-// //******************** lee la cantidad de likes que contiene*****/
-// firebase.database().ref('/posts/' + postId).once('value').then(function (snapshot){
-//   let likes = snapshot.val().likes
-// //********************función de suma 1 a likes */
-// likes = likes + 1
-// })
-// //*******************Actualizar los likes */
-// firebase.database().ref('/posts/' + postId).update({
-//   likes: likes
-// },
-// (error) => {
-//   alert('No puede dar like otravez')
-
-// })
-
-// }
+}
--- src/muro.js
+++ src/muro.js
@@ -16,15 +16,15 @@ let postData = {
   author: null,
   state: 'public',
   starCount: 0
-};
+}
 
 
 
 window.onload = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      postData.uid = user.uid;
-      postData.author = user.displayName;
+      postData.uid = user.uid
+      postData.author = user.displayName
       nameUser.textContent = postData.author
 
 
@@ -32,7 +32,7 @@ window.onload = () => {
     } else {
       location.href = 'index.html'
     }
-  });
+  })
 
 
   //showPosts('Muro')
@@ -63,17 +63,9 @@ const showPosts = (view) => {
   getPost()
     .then(result => {
       //const postsList = unitPost
-      const postsList = result.val();
+      const postsList = result.val()
       for (let unitPost in postsList) {
 
-
-  //       let draw =
-  //         `<div id= card-contend  '${unitPost}'>
-  //       <div class= card-body>
-  // <h6>${postsList[unitPost].author}</h6>
-
-  // <textarea id= card-text '${unitPost}' cols= '100' rows= '6 '>${postsList[unitPost].body}</textarea>
-  //   <br>`
 
         let draw = `<div id= card-contend '${unitPost}'class="card w-75" >
     <h6>${postsList[unitPost].author}</h6>
@@ -81,75 +73,68 @@ const showPosts = (view) => {
       <br>`
 
         if (view === 'Muro') {
-          draw += `<button id=${unitPost}>Me gusta</button>`
-          console.log('hola');
+          draw += `<button id=${unitPost} value='Me gusta'>Me gusta ${postsList[unitPost].starCount}</button>`
+          console.log('hola')
 
-          profileDiv.innerHTML += draw;
-        } else if (view === 'Perfil') {
+          profileDiv.innerHTML += draw
+        }
+
+        else if (view === 'Perfil') {
           draw += `<button id=${unitPost}>Editar</button>
             <button id=${unitPost}>Eliminar</button>`
 
-          posts.innerHTML += draw;
+          posts.innerHTML += draw
 
         }
 
       }
 
-    });
+    })
 
 }
-btnPublic.checked = true;
+btnPublic.checked = true
 
-// const hola = (unitPost) => {
-//   const btnEliminar = document.getElementById('eliminar' + unitPost);
-//         btnEliminar.addEventListener('click', () => {
-//           console.log('hice click a eliminar')
 
-//         })
-// }
-let starCount = 0;
 
 //profileDiv escuchando el evento para llama a la función like
 profileDiv.addEventListener('click', (event) => {
-
-  console.log(profileDiv);
-  starCount++;
-  // starCount = starCount + 1;
+  // console.log(profileDiv) 
 
   const idPost = event.target.id
-  console.log(idPost);
 
   const idUser = postData.uid
-  console.log(idUser);
 
-  if (event.target.nodeName === 'BUTTON' && event.target.textContent === 'Me gusta') {
+  if (event.target.nodeName === 'BUTTON' && event.target.value === 'Me gusta') {
     console.log('llamar a la función de me gusta')
-    likePost = (starCount, idUser, idPost)
+    likePost(idUser, idPost)
+    alert('Le diste Me ggusta 👍')
+    reload_page()
   }
 
+ 
 })
 
 
 
 //divPost escuchando el evento para llamr a las funciones eliminar y editar
 posts.addEventListener('click', (event) => {
-  console.log(posts);
+  console.log(posts)
 
-  //console.log(document.getElementById(`text-${event.target.id}`).textContent);
+  //console.log(document.getElementById(`text-${event.target.id}`).textContent)
 
   const idPost = event.target.id
-  console.log(idPost);
+  console.log(idPost)
 
   const idUser = postData.uid
-  console.log(idUser);
+  console.log(idUser)
 
   if (event.target.nodeName === 'BUTTON' && event.target.textContent === 'Editar') {
-    console.log('llamar a la función de editar posts')
+    console.log('llamar a la fun  ción de editar posts')
     //console.log(postId)
     const textPost = document.getElementById(`text-${event.target.id}`).value
-    //console.log(postData.uid );
+    //console.log(postData.uid )
     const idUser = postData.uid
-    console.log(idUser);
+    console.log(idUser)
 
 
     editPost(idUser, textPost, idPost)
@@ -158,7 +143,7 @@ posts.addEventListener('click', (event) => {
 
 
   } else if (event.target.nodeName === 'BUTTON' && event.target.textContent === 'Eliminar') {
-    console.log('llamar a la función de eliminar posts');
+    console.log('llamar a la función de eliminar posts')
 
     deletePost(idUser, idPost)
     alert('Se elimino correctamente')
@@ -168,55 +153,14 @@ posts.addEventListener('click', (event) => {
 })
 
 
-
-// const btnEdit = document.getElementById(`btn-edit-${unitPost}`)
-
-// btnEdit.addEventListener('click', () => {
-
-
-//   // let newUpdate = document.getElementById(newPost);
-//   let textoNuevo = document.getElementById(`cuerpoPost-${unitPost}`).value
-//   const nuevoPost = {
-//     body: textoNuevo,
-//   };
-
-//   const updatesUser = {};
-//   const updatesPost = {};
-
-//   updatesUser['/user-posts/' + userId + '/' + newPost] = nuevoPost;
-//   updatesPost['/posts/' + newPost] = nuevoPost;
-
-//   firebase.database().ref().update(updatesUser);
-//   firebase.database().ref().update(updatesPost);
-
-// });
-
 //*********función de publicar//**********
 
 btnSave.addEventListener('click', () => {
   const userId = firebase.auth().currentUser.uid
   postData.body = post.value
   const newPost = writeNewPost(postData)
-
-  //reload_page();
-  //const contPost = document.createElement('div');
-  //const textPost = document.createElement('textarea')
-  //textPost.setAttribute("id", newPost);
-
-  //textPost.innerHTML = post.value;
-
-  // btnDelete.addEventListener('click', () => {
-  //   // deletePost(postId, uid) 
-
-
-
-
-  //   // while (contPost.firstChild) contPost.removeChild(contPost.firstChild);
-
-  //   // alert('Eliminar posts!');
-  reload_page();
-
-  // });
+  
+  reload_page() 
 
 
 })
@@ -227,7 +171,7 @@ btnlogout.addEventListener('click', () => {
 })
 
 function reload_page() {
-  window.location.reload();
+  window.location.reload()
 }
 
 
